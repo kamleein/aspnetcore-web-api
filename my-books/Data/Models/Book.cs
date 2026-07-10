c04a1bc8f1aceff231bf262105a2f2aed644d92a
Kamal Raman <kamal.raman2@csgi.com>
2026-07-10 16:47:07 +1000

    Tutorial progression

---
+++ my-books/Data/Models/Book.cs
@@ -0,0 +1,17 @@
+﻿namespace my_books.Data.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public String Description { get; set; }
+        public bool IsRead { get; set; }
+        public DateOnly? DateRead { get; set; }
+        public int? Rate { get; set; }
+        public String Genre { get; set; }
+        public String Author { get; set; }
+        public string CoverUrl { get; set; }
+        public DateOnly? DateAdded { get; set; }
+
+    }
+}
